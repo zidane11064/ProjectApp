67e2645d089d85683fac59473f467e2f6718dca5
Zhang <LZhang19@mdanderson.org>
2019-09-16 17:49:14 -0500

    added authrepository1

--- ProjectApp.API/Controllers/AuthController.cs
+++ ProjectApp.API/Controllers/AuthController.cs
@@ -1,7 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace ProjectApp.API.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class AuthController
     {
-        
     }
+   
 }
