8066521085d2c4880dc1c1b91995c9cc21535556
Zhang <LZhang19@mdanderson.org>
2019-09-16 12:11:35 -0500

    Initial commit for ProjAPP

---
+++ ProjectApp.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using ProjectApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjectApp.API.Data
+{
+    public class DataContext : DbContext
+
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+
+        public DbSet<Value> Values { get; set; }
+        
+    }
+}
---
+++ ProjectApp.API/Models/Value.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace ProjectApp.API.Models
+{
+    public class Value
+    {
+        public int ID { get; set; }
+        public String Name { get; set; }
+    }
+}
